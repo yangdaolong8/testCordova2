4167646838f86a416704e281540b3b8ca2737808
yangdaolong8 <yangdaolong8@163.com>
2018-04-13 11:30:03 +0800

    CordovaLib

--- cordovademo/src/main/java/com/example/nanfang/cordovademo/TestGit.java
+++
@@ -1,4 +0,0 @@
-package com.example.nanfang.cordovademo;
-
-public class TestGit {
-}
---
+++ cordovademo/src/main/java/com/example/nanfang/cordovademo/TestGitActivity.java
@@ -0,0 +1,19 @@
+package com.example.nanfang.cordovademo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class TestGitActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * 初始化view
+     */
+    private void initView() {
+
+    }
+}
